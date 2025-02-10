f36ea4134e8ef435cf536dc682b54ea29a574d0d
nihanth reddy <123232713+nihanth6721@users.noreply.github.com>
2025-02-10 21:42:25 +0530

    Update Vigenere_cipher.py

--- scientific Computing using python/Vigenere_cipher.py
+++ scientific Computing using python/Vigenere_cipher.py
@@ -1,3 +1,7 @@
+#Currently, every single letter is always encrypted with the same letter, depending on the specified offset. What if the offset were different for each letter? 
+#That would be much more difficult to decrypt. 
+#This algorithm is referred to as the Vigenère cipher, where the offset for each letter is determined by another text, called the key
+
 text = 'mrttaqrhknsw ih puggrur'
 custom_key = 'happycoding'
 
